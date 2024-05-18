9eb9cfab9cf497a6a7f567503c2d381690b28923
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-17 23:34:42 -0300

    Fix: Erros de build

--- __pages/(auth)/components/signinGoogle.tsx
+++ __pages/(auth)/components/signinGoogle.tsx
@@ -1,13 +1,14 @@
+"use client";
 import { signIn } from "next-auth/react";
 import React from "react";
 
 const SigninGoogle = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    signIn("google", { redirectTo: "/app" });
+  };
   return (
-    <form
-      action={() => {
-        signIn("google", { redirectTo: "/app" });
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <button type="submit">Sign in Google</button>
     </form>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
+    "./__pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./shared/**/*.{ts,tsx}",
