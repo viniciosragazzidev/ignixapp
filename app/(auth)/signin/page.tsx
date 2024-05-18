f5db8f9d7326645bd8632799b7c9e7faecc0f02d
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-17 22:54:06 -0300

    __

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,8 +1,12 @@
-// import SigninContainer from "@/__pages/(auth)/signin/container/signin-container";
-// import React, { Suspense } from "react";
+import SigninContainer from "@/__pages/(auth)/signin/container/signin-container";
+import React, { Suspense } from "react";
 
 const Signin = () => {
-  return <>{/* <SigninContainer /> */}</>;
+  return (
+    <>
+      <SigninContainer />
+    </>
+  );
 };
 
 export default Signin;
