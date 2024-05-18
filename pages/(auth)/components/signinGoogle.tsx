23efc4c2e68a892e52d6bdcc5e2e43aed4c81407
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-17 21:40:30 -0300

    __

--- pages/(auth)/components/signinGoogle.tsx
+++ pages/(auth)/components/signinGoogle.tsx
@@ -1,14 +1,11 @@
-"use server";
-
-import { signIn } from "@/shared/services/auth";
+import { signIn } from "next-auth/react";
 import React from "react";
 
 const SigninGoogle = () => {
   return (
     <form
-      action={async () => {
-        "use server";
-        await signIn("google", { redirectTo: "/app" });
+      action={() => {
+        signIn("google", { redirectTo: "/app" });
       }}
     >
       <button type="submit">Sign in Google</button>
