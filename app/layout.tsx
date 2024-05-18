ff58251a572d80e6d10f7f583fa7b9b46ce4fd3f
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-17 23:07:09 -0300

    __

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,8 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className={`${inter.className} bg-background`}>
-        {/* <Toaster /> */}
-        {/* <RootProvider> */}
-
-        {children}
-
-        {/* </RootProvider> */}
+        <Toaster />
+        <RootProvider>{children}</RootProvider>
       </body>
     </html>
   );
